29295090656daf258f2b861ff891f3f6e790fbcc
Will Wylie <wyliew@umich.edu>
2023-03-09 14:23:22 -0500

    Initial commit

---
+++ CircleAreaCalculator.java
@@ -0,0 +1,8 @@
+public class CircleAreaCalculator {
+    // making change for midterm exam
+    public static void main(String[] args) {
+        double radius = 10.0; // radius of the circle
+        double area = Math.PI * radius * radius; // formula for area of a circle
+        System.out.println("The area of the circle is " + area);
+    }
+}
