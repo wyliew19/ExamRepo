c1f22c8ca96f2a4265f07615d3c66de2e5a0d6ad
Will Wylie <wyliew@umich.edu>
2023-03-09 14:26:02 -0500

    Changed radius value from 10 to 15

--- CircleAreaCalculator.java
+++ CircleAreaCalculator.java
@@ -1,7 +1,7 @@
 public class CircleAreaCalculator {
     // making change for midterm exam
     public static void main(String[] args) {
-        double radius = 10.0; // radius of the circle
+        double radius = 15.0; // radius of the circle
         double area = Math.PI * radius * radius; // formula for area of a circle
         System.out.println("The area of the circle is " + area);
     }
